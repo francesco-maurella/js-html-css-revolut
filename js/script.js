256f3ecf8a2354a45decfbd3539b68fdd78e1d44
Francesco Maurella <francesco.maurella@live.it>
2021-01-15 15:59:36 +0100

    Up (effetto shadow). Fine.

--- js/script.js
+++ js/script.js
@@ -14,3 +14,25 @@ $(document).ready(function (){
   });
 
 })
+
+// -- VERSTIONE CON FUNCTION (Non funziona, perchè?) --
+// function dropDown(menu){
+//   // dichiarazione sezione richiamata
+//   var thisSection = $(this).find(menu);
+//   // addiziona/rimuovi classe
+//   $('.active').removeClass('active');
+//   thisSection.addClass('active');
+//   $('.active').mouseleave(function(){
+//     // rimuovi classe quando lasci l'area cliccata
+//     thisSection.removeClass('active');
+//   });
+// }
+//
+// $(document).ready(function (){
+//
+//   // assegnazione classe "active", al click
+//   $('.menu-links>li').click(function(){
+//     dropDown('.section-menu');
+//   });
+//
+// })
